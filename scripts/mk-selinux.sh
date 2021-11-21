e9425b051453472793b47ed9855e57e219429edc
Florian Tanner <florian.tanner@adcubum.com>
2021-11-21 21:35:45 +0100

    feat(ddns): replaced cloudflare global key with token

--- scripts/mk-selinux.sh
+++ scripts/mk-selinux.sh
@@ -7,8 +7,10 @@ if is_selinux_enabled; then
   LABEL=$(get_selinux_label)
   DATA=$(get_volume_root)
 
+  # shellcheck disable=SC2035
   selinux_chcon_obj "$LABEL" .env .gitignore *
   selinux_chcon_obj "$LABEL" "$DATA"
+  # shellcheck disable=SC2035
   selinux_chcon_lvl .env .gitignore *
   selinux_chcon_lvl "$DATA"
 fi
