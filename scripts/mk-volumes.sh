5760591dea7dc588d5f29c26e34c05de4eaab89f
Florian Tanner <florian.tanner@adcubum.com>
2022-01-24 15:43:39 +0100

    feat(backup): consolidated restic containers into single instance

--- scripts/mk-volumes.sh
+++ scripts/mk-volumes.sh
@@ -10,8 +10,7 @@ create_volume volumes/nextcloud
 create_volume volumes/nextcloud_db
 create_volume volumes/plex
 create_volume volumes/protonmail-bridge
-create_volume volumes/restic_data
-create_volume volumes/restic_volumes
+create_volume volumes/restic
 create_volume volumes/swag
 create_volume volumes/syncthing
 create_volume volumes/teamspeak
