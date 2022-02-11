cd4dd5d5fddc59f3b0c55e1e06c558627c04564a
Florian Tanner <florian.tanner@adcubum.com>
2022-02-11 19:08:09 +0100

    feat(backup): added script to show last diff

---
+++ images/restic/root/app/restic-diff-last.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/with-contenv bash
+
+[[ -f /config/restic/options ]] && . /config/restic/options
+
+PREV=$(s6-setuidgid abc restic snapshots --compact | tail -4 | head -1 | awk '{print $1}')
+LAST=$(s6-setuidgid abc restic snapshots --compact | tail -3 | head -1 | awk '{print $1}')
+
+s6-setuidgid abc restic diff "${PREV}" "${LAST}"
