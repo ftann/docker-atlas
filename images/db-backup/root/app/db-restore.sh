0e37f78cefda5b75467631cca11d1c759bb6d938
Florian Tanner <florian.tanner@adcubum.com>
2022-02-21 02:05:44 +0100

    feat(backup): added database backup containers

---
+++ images/db-backup/root/app/db-restore.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/with-contenv bash
+
+set -euo pipefail
+
+s6-setuidgid abc mariadb \
+    --host="${DB_HOST}" --port="${DB_PORT}" \
+    --user="${DB_USER}" --password="${DB_PASSWORD}" \
+    "${DB_NAME}" < /config/db/"${DB_NAME}".sql
