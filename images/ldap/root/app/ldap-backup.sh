963a954c08147709e1965ed677590e0c16fa4697
Florian Tanner <3204445+ftann@users.noreply.github.com>
2022-12-06 16:39:42 +0100

    fix(ldap): use root user to back up and restore data

--- images/ldap/root/app/ldap-backup.sh
+++ images/ldap/root/app/ldap-backup.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 . /app/env.sh
 . /app/string.sh
 
-file_env 'LDAP_BIND_PASSWORD'
+file_env 'LDAP_ROOT_PASSWORD'
 
 # shellcheck disable=SC2046
 LDAP_DOMAIN="$(join_by "," $(prepend_each "dc=" $(split "${DOMAIN}")))"
@@ -15,6 +15,6 @@ export LDAPTLS_REQCERT
 
 ldapsearch \
     -H "ldap://localhost" \
-    -D "cn=${LDAP_BIND_USER},${LDAP_DOMAIN}" -w "${LDAP_BIND_PASSWORD}" \
+    -D "cn=${LDAP_ROOT},${LDAP_DOMAIN}" -w "${LDAP_ROOT_PASSWORD}" \
     -Z -b "${LDAP_DOMAIN}" \
     -LLL >"/config/${DOMAIN}.ldif"
