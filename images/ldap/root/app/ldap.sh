cb82b9168176e669debaf8190dd70498d832ecf5
florian.tanner <3204445+ftann@users.noreply.github.com>
2023-04-07 23:01:28 +0200

    feat(sso): replace openldap with lldap

--- images/ldap/root/app/ldap.sh
+++
@@ -1,9 +0,0 @@
-create_password_hash() {
-  local input=$1
-  slappasswd -n -h "{SSHA}" -s "${input}"
-}
-
-get_organization() {
-  local dn=$1
-  echo "${dn}" | cut -d, -f1 | cut -d= -f2
-}
--- images/ldap/root/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-/app/init.sh
-
-exec "$@"
